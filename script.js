4ef8e6909585b1b5ce8a11b233f27cbf683b019e
ogasawara kakeru <kakeru19870423@gmail.com>
2026-08-23 13:30:56 +0900

    NOT and BOOLEAN operator

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(0 || null || undefined);
+//1. If statement
+//2. NOT ! => CONVERTING A VALUE TO IT'S TRUE BOOLEAN TYPE.
+console.log(!!"TEST");
+console.log(!!0);
+
+//3. Boolean
+console.log(Boolean('TEST'));
+console.log(Boolean(undefined));
