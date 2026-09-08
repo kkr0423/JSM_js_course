e983ea2fb1ef2a89870a756b34f80cfa8f8ae627
ogasawara kakeru <kakeru19870423@gmail.com>
2026-09-09 04:27:46 +0900

    Advanced Array

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ console.log(names);//['John', 'Jane', 'Jim', 'Jill'];
 
 names.splice(1, 0, "Jenny");
 console.log(names);//['John', 'Jenny', 'Jane', 'Jim', 'Jill'];
+
+const moreNames = ['Mary', 'Mark', 'Matt'];
+const allNames = names.concat(moreNames);
+console.log(allNames);//['John', 'Jenny', 'Jane', 'Jim', 'Jill', 'Mary', 'Mark', 'Matt'];
+
+console.log(names.indexOf('Jane'));//1
+
+console.log(names.lastIndexOf('Jane'));//1
+
+console.log(names.includes('Jane'));//true
+
+const foundName = names.find(name => name.length > 4);
+console.log(foundName);//'Jenny'
+
+const filteredNames = names.filter(name => name.length > 4);
+console.log(filteredNames);//['Jenny', 'Jane', 'Jim', 'Jill'];
+
